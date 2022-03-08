fa77fc5862e2c740daff8f1dcca2a7f9e9959049
Anzor <Satuevone@gmail.com>
2022-03-08 18:49:04 +0300

    module2-task1

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Information = {
+  FILM_NAME: 'zzzz',
+  FILM_TYPE: 'Dramma',
+  FILM_YEAR: 2011,
+};
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      name = {Information.FILM_NAME}
+      year = {Information.FILM_YEAR}
+      type = {Information.FILM_TYPE}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
